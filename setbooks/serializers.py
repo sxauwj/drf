83b2b5bf0ef35c74c2958811a145d8e5f66c2f1d
wangjie <wangjiepy@163.com>
2018-11-29 18:35:15 +0800

    使用drf定义serializer类

---
+++ setbooks/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import BookInfo
+
+
+# 定义序列化器类
+class BookInfoSerializer(serializers.ModelSerializer):
+    """图书序列化器"""
+
+    class Meta:
+        model = BookInfo
+        fields = '__all__'
+
+
+# 定义序列化器类
+class BookSerializer(serializers.Serializer):
+    # 该字段只允许序列化输出
+    id = serializers.IntegerField(read_only=True)
+    btitle = serializers.CharField(
+        min_length=1,
+        max_length=15,
+        error_messages={
+            'min_length': '书名要大于3个字符',
+            'max_length': '书名要小于15个字符'
+        }
+    )
+    # 该字段只允许反序列化输入
+    bpub_date = serializers.DateField(write_only=True)
--- setbooks/urls.py
+++ setbooks/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import  url
 from . import views
-
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
-    url(r'^queries/$',views.BookInfoView.as_view()),
-    url(r'^options/(?P<pk>\d+)/$',views.BookOptionsView.as_view()),
+    # url(r'^queries/$',views.BookInfoView.as_view()),
+    # url(r'^options/(?P<pk>\d+)/$',views.BookOptionsView.as_view()),
 ]
+# 定义可以处理视图的路由器
+router = DefaultRouter()
+# 向路由器注册视图集
+router.register(r'queries',views.BookInfoViewSet)
+urlpatterns += router.urls
