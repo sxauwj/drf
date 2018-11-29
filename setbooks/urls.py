ebcd98714b737941227b4c83266fe117618dc6fb
wangjie <wangjiepy@163.com>
2018-11-29 18:45:01 +0800

    序列化一个对象

--- setbooks/urls.py
+++ setbooks/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import  url
+from django.conf.urls import url
 from . import views
 from rest_framework.routers import DefaultRouter
+
 urlpatterns = [
     # url(r'^queries/$',views.BookInfoView.as_view()),
     # url(r'^options/(?P<pk>\d+)/$',views.BookOptionsView.as_view()),
+    url('^books/$', views.BookView.as_view())
 ]
 # 定义可以处理视图的路由器
 router = DefaultRouter()
 # 向路由器注册视图集
-router.register(r'queries',views.BookInfoViewSet)
+router.register(r'queries', views.BookInfoViewSet)
 urlpatterns += router.urls
